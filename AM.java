70e9ac86d4ba35a6940bd11de313a8358f28b2b5
cathymavr <t8230092@aueb.gr>
2024-11-11 19:50:55 +0200

    Cathy's AM

--- AM.java
+++ AM.java
@@ -4,4 +4,11 @@ public class AM{
     System.out.println("8230101");
     System.out.println("8230124");
  }
+public class AM {
+   public static void main(String [] args){
+      System.out.println("8230028");
+      System.out.println("8230101");
+      System.out.println("8230124");
+      System.out.println("8230092");
+   }
 }
