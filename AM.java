3b5c991cbdbb3bc7c3f4ccfb9a2d4bd0697f83d5
Cathy Mavrou <t8230092@aueb.gr>
2024-11-12 14:36:40 +0200

    Second try

--- AM.java
+++ AM.java
@@ -1,9 +1,3 @@
-public class AM{
- public static void main(String [] args){
-    System.out.println("8230028");
-    System.out.println("8230101");
-    System.out.println("8230124");
- }
 public class AM {
    public static void main(String [] args){
       System.out.println("8230028");
