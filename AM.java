4c6f331fecf62b6aaa6054a97cd19f54b72ecb8c
Aliki Potamianaki <t8230124@aueb.gr>
2024-11-17 19:43:01 +0200

    my update

--- AM.java
+++ AM.java
@@ -2,11 +2,11 @@ public class AM {
    public static void main(String [] args){
       System.out.println("8230028");
       System.out.println("8230101");
-      System.out.println("8230124");
       System.out.println("8230092");
       System.out.println("8230030");
       System.out.println("8230131");
       System.out.println("8230132");
       System.out.println("8230017");
+      System.out.println("8230124");
    }
 }
